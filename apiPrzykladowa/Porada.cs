b51d503030bb2518800c4ab4d91773659f4cca2a
Damian Raczek <draczekprojekt@gmail.com>
2024-11-29 09:39:37 +0100

    Porada class and LosujPorade static async function created.

---
+++ apiPrzykladowa/Porada.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apiPrzykladowa
+{
+    public static class Porada
+    {
+        public static async Task<string> LosujPorade()
+        {
+            string apiUrl = "https://api.adviceslip.com/advice";
+            using (HttpClient klient = new HttpClient()) 
+            {
+                HttpResponseMessage odpowiedz = await klient.GetAsync(apiUrl);
+                odpowiedz.EnsureSuccessStatusCode();
+                return JObject.Parse(await odpowiedz.Content.ReadAsStringAsync())["slip"]["advice"].ToString();
+            }
+        }
+    }
+}
